29aa07d8cb065c71fa3339516e0f813fca953239
GuidoAlfaro <ardayaalfaro@gmail.com>
2025-05-17 17:02:24 -0400

    TDD: alternar turnos fallo

--- src/test/java/bo/edu/ucb/junit/TresEnRayaTest.java
+++ src/test/java/bo/edu/ucb/junit/TresEnRayaTest.java
@@ -10,4 +10,20 @@ public class TresEnRayaTest {
         JuegoTresEnRaya juego = new TresEnRaya();
         assertEquals("X", juego.obtenerJugadorActual());
     }
+
+    @Test
+    void elTurnoDebeAlternarEntreXyO() {
+        JuegoTresEnRaya juego = new TresEnRaya();
+
+        assertEquals("X", juego.obtenerJugadorActual(), "Debe empezar jugando X");
+
+        juego.ponerFicha(0, 0);
+
+        assertEquals("+", juego.obtenerJugadorActual(), "Después de X, debe jugar +");
+
+        juego.ponerFicha(1, 1);
+
+        assertEquals("+", juego.obtenerJugadorActual(), "Después de +, debe jugar X");
+    }
+
 }
